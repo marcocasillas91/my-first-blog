cf770d5fbfd1e2c7b761c7f61e61491f3dfb2e5b
Marco Casillas <marco.casillas91@gmail.com>
2019-05-30 17:31:05 -0500

    Added views to create/edit blog post inside the site

---
+++ blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from .models import Post
+
+class PostForm(forms.ModelForm):
+	"""docstring for PostForm"""
+	
+	class Meta:
+		"""docstring for Meta"""
+		model  = Post
+		fields = ('title' , 'text',)
+			
+		
